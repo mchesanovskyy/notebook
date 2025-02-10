8a0a27a712b9e0753f3d819823b20754c227b72e
mchesanovskyy <nick.chesanovskyy@gmail.com>
2025-02-10 14:58:50 +0200

    refactored data folder

--- Notebook.WebApi/Data/Entities/Note.cs
+++ Notebook.WebApi/Data/Entities/Note.cs
@@ -12,11 +12,3 @@ public class Note
 
     public virtual NoteCategory NoteCategory { get; set; }
 }
-
-public class NoteCategory
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    public virtual ICollection<Note> Notes { get; set; }
-}
---
+++ Notebook.WebApi/Data/Entities/NoteCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Notebook.WebApi.Data.Entities;
+
+public class NoteCategory
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public virtual ICollection<Note> Notes { get; set; }
+}
